1b8e010a618ee7ef86cc59e3935c0ebf95b568cd
Bruno Chirez <bchirez@carglass.fr>
2017-09-07 18:18:09 +0200

    Fix PHP CheckStyle

--- features/bootstrap/FeatureContext.php
+++ features/bootstrap/FeatureContext.php
@@ -7,63 +7,64 @@ use Behat\Gherkin\Node\TableNode;
 use Domain\Math\Calculator;
 
 /**
-* Defines application features from the specific context.
-*/
+ * Defines application features from the specific context.
+ */
 class FeatureContext implements Context
 {
-	private $result;
-	/**
-	* Initializes context.
-	*
-	* Every scenario gets its own context instance.
-	* You can also pass arbitrary arguments to the
-	* context constructor through behat.yml.
-	*/
-	public function __construct()
-	{
-		$this->result = null;
-		$this->calculator = new Calculator();
-	}
+    private $result;
 
-	/**
-	* @When I add :arg1 and :arg2
-	*/
-	public function iAddAnd($arg1, $arg2)
-	{
-		$this->result = $this->calculator->add($arg1, $arg2);
-	}
+    /**
+     * Initializes context.
+     *
+     * Every scenario gets its own context instance.
+     * You can also pass arbitrary arguments to the
+     * context constructor through behat.yml.
+     */
+    public function __construct()
+    {
+        $this->result = null;
+        $this->calculator = new Calculator();
+    }
 
-	/**
-	* @Then I get :arg1
-	*/
-	public function iGet($arg1)
-	{
-		if($arg1 != $this->result) {
-			throw new \Exception();
-		}
-	}
+    /**
+     * @When I add :arg1 and :arg2
+     */
+    public function iAddAnd($arg1, $arg2)
+    {
+        $this->result = $this->calculator->add($arg1, $arg2);
+    }
 
-	/**
-	* @When I substract :arg1 and :arg2
-	*/
-	public function iSubstractAnd($arg1, $arg2)
-	{
-		$this->result = $this->calculator->substract($arg1, $arg2);
-	}
+    /**
+     * @Then I get :arg1
+     */
+    public function iGet($arg1)
+    {
+        if ($arg1 != $this->result) {
+            throw new \Exception();
+        }
+    }
 
-	/**
-	* @When I multiply :arg1 and :arg2
-	*/
-	public function iMultiplyAnd($arg1, $arg2)
-	{
-		$this->result = $this->calculator->multiply($arg1, $arg2);
-	}
+    /**
+     * @When I substract :arg1 and :arg2
+     */
+    public function iSubstractAnd($arg1, $arg2)
+    {
+        $this->result = $this->calculator->substract($arg1, $arg2);
+    }
 
-	/**
-	* @When I divde :arg1 and :arg2
-	*/
-	public function iDivdeAnd($arg1, $arg2)
-	{
-		$this->result = $this->calculator->divide($arg1, $arg2);
-	}
+    /**
+     * @When I multiply :arg1 and :arg2
+     */
+    public function iMultiplyAnd($arg1, $arg2)
+    {
+        $this->result = $this->calculator->multiply($arg1, $arg2);
+    }
+
+    /**
+     * @When I divde :arg1 and :arg2
+     */
+    public function iDivdeAnd($arg1, $arg2)
+    {
+        $this->result = $this->calculator->divide($arg1, $arg2);
+    }
 }
--- src/Domain/Math/Calculator.php
+++ src/Domain/Math/Calculator.php
@@ -4,27 +4,34 @@ declare(strict_types=1);
 
 namespace Domain\Math;
 
-final class Calculator {
-	public function add($number1, $number2): int {
-		// check dead code
-		if(false) {
-			return 1;
-		}
-		return $number1 + $number2;
-	}
+final class Calculator
+{
+    public function add($number1, $number2): int
+    {
+        // check dead code
+        if (false) {
+            return 1;
+        }
 
-	public function substract($number1, $number2) {
-		return $number1 - $number2;
-	}
+        return $number1 + $number2;
+    }
 
-	public function multiply($number1, $number2) {
-		return $number1 * $number2;
-	}
+    public function substract($number1, $number2)
+    {
+        return $number1 - $number2;
+    }
 
-	public function divide($number1, $number2) {
-		if($number2 === 0) {
-			throw new \InvalidArgumentException('Division by zero is not possible');
-		}
-		return $number1 / $number2;
-	}
+    public function multiply($number1, $number2)
+    {
+        return $number1 * $number2;
+    }
+
+    public function divide($number1, $number2)
+    {
+        if ($number2 === 0) {
+            throw new \InvalidArgumentException('Division by zero is not possible');
+        }
+
+        return $number1 / $number2;
+    }
 }
