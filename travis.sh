89c7a7dae4c9e55e61b45c5d4f26408d67db4887
Deamon <bdurand-dev@outlook.com>
2017-09-07 16:13:10 +0200

    update travis script

--- travis.sh
+++ travis.sh
@@ -6,7 +6,9 @@ GH_SONARQ_PARAMS=""
 # When run on Travis CI, env var TRAVIS_PULL_REQUEST either contains PR number (for PR builds) or "false" (for push builds).
 # Locally this env var is not set. Test: if variable is not empty and is not equal "false"
 echo "travis_secure_env= ${TRAVIS_SECURE_ENV_VARS}"
-if [ ! -z "$TRAVIS_PULL_REQUEST" ] && [ "${TRAVIS_PULL_REQUEST}" != "false" ] && [ "${TRAVIS_SECURE_ENV_VARS}" == "true" ]; then
+echo "SQ_URL = ${SQ_URL}"
+#if [ ! -z "$TRAVIS_PULL_REQUEST" ] && [ "${TRAVIS_PULL_REQUEST}" != "false" ] && [ "${TRAVIS_SECURE_ENV_VARS}" == "true" ]; then
+if [ ! -z "$TRAVIS_PULL_REQUEST" ] && [ "${TRAVIS_PULL_REQUEST}" != "false" ]; then
     echo "PR build. Will execute SonarQube preview scan"
     GH_SONARQ_PARAMS="-Dsonar.analysis.mode=preview -Dsonar.host.url=$SQ_URL -Dsonar.github.oauth=$GH_TOKEN -Dsonar.github.repository=$TRAVIS_REPO_SLUG -Dsonar.github.pullRequest=$TRAVIS_PULL_REQUEST -Dsonar.projectKey=fr.carglass.travis-test:test-php -Dsonar.projectName=Travis-Php-Test -Dsonar.projectVersion=0.1 -Dsonar.sources=src -Dsonar.language=php"
 fi
@@ -17,4 +19,6 @@ fi
 #    GH_SONARQ_PARAMS="-Dsonar.host.url=$SQ_URL -Dsonar.jdbc.url=$SQ_JDBC_URL -Dsonar.jdbc.driverClassName=org.postgresql.Driver -Dsonar.jdbc.user=$SQ_JDBC_USER -Dsonar.jdbc.password=$SQ_JDBC_PASSWORD"
 #fi
 
-sonar-scanner-3.0.3.778-linux/bin/sonar-scanner ${GH_SONARQ_PARAMS}
+if [ ${GH_SONARQ_PARAMS} != "" ]; then
+    sonar-scanner-3.0.3.778-linux/bin/sonar-scanner ${GH_SONARQ_PARAMS}
+fi
